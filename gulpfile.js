110d200bd03856055af3638997606ac03cf60b92
ernest-lamptey <1ernestlamptey@gmail.com>
2023-10-26 17:01:06 +0200

    setup partials and folders

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass')) //invoke to compile sass to css
 
 function buildStyles() {
-    return src('index.scss')
+    return src('custom/**/*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['index.scss'], buildStyles)
+    watch(['custom/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
