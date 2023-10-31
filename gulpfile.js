186b698bd891b3519981d25972dc3a5394b6094a
ernest-lamptey <1ernestlamptey@gmail.com>
2023-10-31 03:17:39 +0100

    add purgecss functionality into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass')) //invoke to compile sass to css
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
     return src('custom/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({content: ['*.html']}))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['custom/**/*.scss'], buildStyles)
+    watch(['custom/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
